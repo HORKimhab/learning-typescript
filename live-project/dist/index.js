cbcec52fe6b5df013db142fa428ed75f5d8dfcaa
HKimhab <hkimhab@mango-byte.com>
2024-03-04 14:26:45 +0700

    feat: private

--- live-project/dist/index.js
+++ live-project/dist/index.js
@@ -2,3 +2,14 @@
 console.log('Typescript is here');
 console.log('Other from TypeScript with live-project');
 console.log('Live Testing...');
+/* Class TypeScript */
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = "Phnom Penh";
+    }
+}
+// const HKimahb = new User(name: 'hkimhab', email: 'hkimhab@gmail.com') // Error 
+const user = new User('HKimhab', 'hkimhab@gmail.com');
+// user.city = "Phnom Pneh"
--- live-project/src/index.ts
+++ live-project/src/index.ts
@@ -4,15 +4,14 @@ console.log('Live Testing...');
 
 /* Class TypeScript */
 class User { 
-    email: string 
-    name: string
-    city: string = ''
-    constructor(email: string, name: string){
-        this.email = email; 
-        this.name = name; 
-    }
+    readonly city: string = "Phnom Penh"
+    constructor(
+        public email: string, 
+        public name: string, 
+        // private userId: string
+    ){}
 }
 
 // const HKimahb = new User(name: 'hkimhab', email: 'hkimhab@gmail.com') // Error 
 const user = new User('HKimhab', 'hkimhab@gmail.com');
-user.city = "Phnom Pneh"
+// user.city = "Phnom Pneh"
