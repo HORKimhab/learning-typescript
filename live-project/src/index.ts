83bdccaa23b5db5e6d109fa3ff0a468cbb51f1c9
HKimhab <hkimhab@mango-byte.com>
2024-03-04 14:16:01 +0700

    feat: init class typescript

--- live-project/src/index.ts
+++ live-project/src/index.ts
@@ -1,3 +1,18 @@
 console.log('Typescript is here');
 console.log('Other from TypeScript with live-project');
 console.log('Live Testing...');
+
+/* Class TypeScript */
+class User { 
+    email: string 
+    name: string
+    city: string = ''
+    constructor(email: string, name: string){
+        this.email = email; 
+        this.name = name; 
+    }
+}
+
+// const HKimahb = new User(name: 'hkimhab', email: 'hkimhab@gmail.com') // Error 
+const user = new User('HKimhab', 'hkimhab@gmail.com');
+user.city = "Phnom Pneh"
