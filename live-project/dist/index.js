78144acafd1147ada9c9cfcb1508f17f966853bf
HKimhab <hkimhab@mango-byte.com>
2024-03-04 13:39:40 +0700

    ignore: add file gitignore file

---
+++ live-project/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('Typescript is here');
+console.log('Other from TypeScript with live-project');
+console.log('Live Testing...');
---
+++ live-project/src/index.ts
@@ -0,0 +1,3 @@
+console.log('Typescript is here');
+console.log('Other from TypeScript with live-project');
+console.log('Live Testing...');
